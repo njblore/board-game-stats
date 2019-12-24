8d74b458454a7945daf009b35b86d2683692fe23
njblore <njblore@outlook.com>
2019-12-24 09:29:48 +0000

    Updates

---
+++ precache-manifest.f15f4528e72210bbc79af9ea1825ff82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6bb538fab83b0585372e40869a54fa",
+    "url": "/project-yule/index.html"
+  },
+  {
+    "revision": "7671b4284569a1d0052f",
+    "url": "/project-yule/static/css/main.7c744dc4.chunk.css"
+  },
+  {
+    "revision": "a8ed4b6c03768a356d2e",
+    "url": "/project-yule/static/js/2.6533a228.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/project-yule/static/js/2.6533a228.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7671b4284569a1d0052f",
+    "url": "/project-yule/static/js/main.5c6b2e26.chunk.js"
+  },
+  {
+    "revision": "eebba6e260a48200ce5c",
+    "url": "/project-yule/static/js/runtime-main.243a31e2.js"
+  },
+  {
+    "revision": "64787d7ad665af3b169b3e2f8498b4db",
+    "url": "/project-yule/static/media/DOMII___.64787d7a.TTF"
+  },
+  {
+    "revision": "0003423bdc1597be79b69dcf7026db58",
+    "url": "/project-yule/static/media/DOMISC__.0003423b.TTF"
+  }
+]);
